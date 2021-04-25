608f166dfeb75b6b569a53035258c0373e146e52
jtirkkon <jouni.tirkkonen@gmail.com>
2021-04-25 20:41:22 +0300

    tasks ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { AgGridReact } from 'ag-grid-react'; 
+import { AgGridReact } from 'ag-grid-react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCar from './components/AddCar';
+import EditCar from './components/EditCar';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import './App.css';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+//https://carshopjt.herokuapp.com/
 
 function App() {
   const [cars, setCars] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState('');
+
+  const openSnackbar = () => {
+    setOpen(true);
+  }
+
+  const closeSnackbar = () => {
+    setOpen(false);
+  }
 
   useEffect(() => {
     fetchCars();
@@ -22,35 +38,111 @@ function App() {
     .catch(err => console.error(err))
   }
 
+  const deleteCar = (url) => {
+    if (window.confirm('Are you sure?')) {
+      fetch(url, { method: 'DELETE'})
+      .then(response => {
+        if (response.ok) {
+          setMsg('Car deleted');
+          openSnackbar();
+          fetchCars();
+        }
+        else {
+          alert("Something went wrong!");
+        }
+      })
+      .catch(err => console.error(err))
+    }
+  }
+
+  const addCar = (newCar) => {
+    fetch('https://carstockrest.herokuapp.com/cars', {
+      method: 'POST',
+      body: JSON.stringify(newCar),
+      headers: { 'Content-type' : 'application/json' }
+    })
+    .then(response => {
+      if (response.ok)
+        fetchCars();
+      else 
+        alert("Something went wrong!");
+    })
+    .catch(err => console.error(err))
+  }
+
+  const editCar = (url, updatedCar) => {
+    fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(updatedCar),
+      headers: { 'Content-type' : 'application/json' }  
+    })
+    .then(response => {
+      if (response.ok) {
+        setMsg('Car edited succesfully');
+        openSnackbar();
+        fetchCars();
+      }
+      else 
+        alert("Something went wrong!");
+    })
+    .catch(err => console.error(err)) 
+
+  }
+
   const columns = [
     {field: 'brand', sortable: true, filter: true},
     {field: 'model', sortable: true, filter: true},
     {field: 'color', sortable: true, filter: true},
     {field: 'fuel', sortable: true, filter: true},
     {field: 'year', sortable: true, filter: true, width: 100},
-    {field: 'price', sortable: true, filter: true, width: 120}
+    {field: 'price', sortable: true, filter: true, width: 120},
+    {
+      headerName: '',
+      field: '_links.self.href',
+      width: 100,
+      cellRendererFramework: params =>
+        <EditCar link={params.value} car={params.data} editCar={editCar}/>
+    },
+    {
+      headerName: '', 
+      field: '_links.self.href',
+      width: 100,
+      cellRendererFramework: params => 
+      <IconButton color='secondary' onClick={() => deleteCar(params.value)}>
+        <DeleteIcon />
+      </IconButton>
+    }
   ]
 
   return (
     <div className="App">
-      <Toolbar position='static'>
-        <AppBar>
+      <AppBar position='static'>
+        <Toolbar>
           <Typography variant='h6'>
             Carshop
           </Typography>
-        </AppBar>
-      </Toolbar>
+        </Toolbar>
+      </AppBar>
+      <AddCar addCar={addCar}/>
       <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto'}}>
         <AgGridReact
           rowData={cars}
           columnDefs={columns}
           pagination={true}
           paginationPageSize={8}
+          suppressCellSelection={true}
         />
       </div>
-      
+      <Snackbar 
+        open={open}
+        message={msg}
+        autoHideDuration={3000}
+        onClose={closeSnackbar}
+      />
     </div>
   );
 }
 
 export default App;
+
+
